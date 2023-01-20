a5e8196354e5c2a431d6dbe2a8623c074fe3d982
tonimjdev <tonimdev@gmail.com>
2023-01-20 12:21:38 +0100

    106 - Componente para buscar país

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
 import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
 import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
+import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
 
 const routes:  Routes = [
     {
         path: '',
-        component: PorCapitalComponent,
+        component: PorPaisComponent,
         pathMatch: 'full'
     },
     {
